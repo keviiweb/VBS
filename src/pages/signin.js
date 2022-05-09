f220a516c734c171fd2bd5fb319961b486b9551f
Hikoya <hikoyanamikaze@gmail.com>
2022-05-09 14:51:04 +0800

    Update signin.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,50 +1,58 @@
+import * as React from "react";
 import {
   FormControl,
   FormLabel,
   FormHelperText,
   Input,
-  Button
-} from '@chakra-ui/react';
-import * as yup from 'yup';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { signIn } from "next-auth/react";
+  Button,
+  Center, 
+  Flex
+} from "@chakra-ui/react";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const schema = yup.object().shape({
-  email: yup.string().email().required(),
+  email: yup.string().email().required()
 });
 
 export default function LoginForm() {
-  const { register, handleSubmit } = useForm({
-    mode: 'onBlur',
-    resolver: yupResolver(schema),
+  const { register, handleSubmit, errors } = useForm({
+    mode: "onBlur",
+    resolver: yupResolver(schema)
   });
 
-  const onSubmit = (values) => signIn("email", { email: values.email });;
+  const onSubmit = (values) => console.log(values.email);
 
   return (
-    <form style={{ width: 350 }}>
+      <Flex justify="center" h="100vh" w="100vw" align="center">
+      <Center w="100%">
+      <form style={{ width: 350 }}>
       <FormControl
-        p='4'
+        p="4"
         isRequired
       >
         <FormLabel>Email</FormLabel>
-        <Input type='email' name='email' placeholder='Email' ref={register} />
+        <Input type="email" name="email" placeholder="Email" ref={register} />
         <FormHelperText>
-          Please enter your school email ending with @u.nus.edu
+          Please use your school email ending with @u.nus.edu
         </FormHelperText>
-      </FormControl>
+      </FormControl>  
       <Button
         onClick={handleSubmit(onSubmit)}
-        p='4'
-        mx='4'
-        mt='6'
-        w='90%'
-        colorScheme='blue'
-        variant='solid'
+        p="4"
+        mx="4"
+        mt="6"
+        w="90%"
+        colorScheme="blue"
+        variant="solid"
       >
         Login
       </Button>
     </form>
+      </Center>
+    </Flex>
+
+   
   );
 }
