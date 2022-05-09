ed3706f5d33df7136a7712aee8951a88c65e0844
Hikoya <hikoyanamikaze@gmail.com>
2022-05-10 00:25:18 +0800

    Update helper.js

--- src/constants/helper.js
+++ src/constants/helper.js
@@ -2,7 +2,7 @@ import { prisma } from './db';
 import { getSession } from "next-auth/react";
 
 export const getAllLocation = async () => {
-    const {data: session} = await getSession();
+    const session = await getSession();
 
     if (session) {
         const locations = await prisma.venue.findMany();  
