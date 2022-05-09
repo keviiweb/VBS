1e083688ef430d52832837e1ab64f96e4143e236
Hikoya <hikoyanamikaze@gmail.com>
2022-05-09 16:46:18 +0800

    Update index.js

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,5 +2,7 @@ import Image from 'next/image';
 import { constants } from '../../constants';
 
 export const Logo = (props) => (
-  <Image alt="Vercel logo" src={constants["keviilogo"]} />
+  <div style={{width: '200px', height: '200px', position: 'relative'}}>
+      <Image alt="Vercel logo" src={constants["keviilogo"]} layout='fill'/>
+  </div>  
 )
