ed7e7a7c3550a764c38c60a47e93c7e770528881
Hikoya <hikoyanamikaze@gmail.com>
2022-05-11 01:23:40 +0800

    Testing of calendar view

--- src/components/CalendarWidget.js
+++ src/components/CalendarWidget.js
@@ -1,7 +1,39 @@
 import Calendar from "react-calendar";
-import { useState } from "react";
+import React, { useState } from "react";
+import 'react-calendar/dist/Calendar.css';
+import "@styles/Calendar.css";
+import { monthNamesFull } from "@constants/months";
 
 export default function CalendarWidget() {
-  const [value, onChange] = useState(new Date());
-  return <Calendar onChange={onChange} value={value} />;
+  const minDate = new Date();
+  minDate.setDate(new Date().getDate() + Number(process.env.CALENDAR_MIN_DAY));
+
+  const maxDate = new Date();
+  maxDate.setDate(new Date().getDate() + Number(process.env.CALENDAR_MAX_DAY));
+
+  const [date, setDate] = useState(new Date());
+  const handleChange = (date) => {
+    setDate(date);
+  }
+
+  const displayLabel = (date, label, locale, view) => {
+    var month = monthNamesFull[date.getMonth()];
+    var year = date.getFullYear();
+    if (view === "month") {
+      return `${month}`;
+    }
+    return `${year}`;
+  }
+
+  return (
+    <Calendar
+      onChange={handleChange}
+      value={date}
+      minDate={minDate}
+      minDetail="year"
+      next2Label={null}
+      prev2Label={null}
+      navigationLabel={displayLabel}
+    />
+  );
 }
