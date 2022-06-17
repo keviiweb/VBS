e3954be7f3116e805dc5595cbb9c7d38b8e1a275
Hikoya <hikoyanamikaze@gmail.com>
2022-06-17 22:45:52 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: "kevii-vbs",
-      script: "./node_modules/next/dist/bin/next",
-      args: "start -p " + (process.env.PORT || 3000),
+      name: 'kevii-vbs',
+      script: './node_modules/next/dist/bin/next',
+      args: 'start -p ' + (process.env.PORT || 3000),
       watch: false,
       autorestart: true,
     },
